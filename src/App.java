bb5a730bff9becf0b9b4b339f74fd1008a8966c7
chalermch <chalermchon970@gmail.com>
2022-12-17 16:27:56 +0700

    version 6

--- src/App.java
+++ src/App.java
@@ -1,11 +1,14 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import database.DatabaseHandler;
 import model.Employee;
 import model.EmployeeTable;
 import model.Menu;
 import model.MenuTable;
+import model.ReportSellDaliy;
+import model.ReportTable;
 import model.SaleOrder;
 import model.SaleOrderTable;
 import model.Timestamp;
@@ -21,6 +24,8 @@ public class App {
         Date logOut = dt.parse(date[1]);
     
         DatabaseHandler dbHandler = new DatabaseHandler();
+        List<ReportSellDaliy> reportList = ReportTable.findAllReportOfSell(dbHandler);
+        printAllReport(reportList);
         //Employee emp = new Employee(2,"John", "Merry", "111", 12, "144/12", "0955049358", "work", 1);
         //EmployeeTable.insertEmployee(dbHandler, emp);
        // Menu menu = new Menu(2, "hee", 1500);
@@ -33,10 +38,22 @@ public class App {
         Date logIn = dt.parse(date[0]);
         Date logOut = dt.parse(date[1]);
         System.out.println(logIn);
-*/
+
 
         Timestamp time = new Timestamp(2, "2013-07-22 15:35:07","2013-07-23 15:35:07");
         TimestampTable.insertTimestamp(dbHandler, time);
-        
+*/        
+    }
+
+    public static void printAllReport(List<ReportSellDaliy> reportList) {
+        for(ReportSellDaliy report : reportList) {
+           System.out.println(report.getOrderID() + " ");
+           System.out.println(report.getEmployeeID() + " ");
+           System.out.println(report.getFoodName() + " ");
+           System.out.println(report.getFoodPrice() + " ");
+           System.out.println(report.getQuantity() + " ");
+           System.out.println(report.getTotalPrice() + " ");
+           System.out.println(report.getSaleTimestamp() + " ");
+       }
     }
 }
---
+++ src/model/ReportTable.java
@@ -0,0 +1,84 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import database.DatabaseHandler;
+
+public class ReportTable {
+
+
+    public static ReportSellDaliy findOrderById(DatabaseHandler dbHandler, int orderid) throws SQLException {
+        String sql = "SELECT OrderID, EmployeeID, FoodName, FoodPrice, Quantity, TotalPrice, SaleTimestamp FROM SaleOrder AS s , Menu AS m WHERE s.FoodID = m.FoodID ORDER BY OrderID ASC";
+        ResultSet rs;
+        ReportSellDaliy report = null;
+        rs = dbHandler.query(sql, orderid);
+        if (rs.next()) {
+            report = new ReportSellDaliy();
+            report.setOrderID(rs.getInt("OrderID"));
+            report.setEmployeeID(rs.getInt("EmployeeID"));
+            report.setFoodName(rs.getString("FoodName"));
+            report.setFoodPrice(rs.getInt("FoodPrice"));
+            report.setQuantity(rs.getInt("Quantity"));
+            report.setTotalPrice(rs.getDouble("TotalPrice"));
+            report.setSaleTimestamp(rs.getString("SaleTimestamp"));
+
+        }
+        return report;
+
+    }
+
+    // การหาค่าในตารางโดยหาชื่อที่ต้องการ
+    public static ArrayList<ReportSellDaliy> findOrderByTime(DatabaseHandler dbHandler, String time)
+            throws SQLException {
+        String sql = "SELECT OrderID, EmployeeID, FoodName, FoodPrice, Quantity, TotalPrice, SaleTimestamp FROM SaleOrder AS s , Menu AS m WHERE s.FoodID = m.FoodID AND s.SaleTimestamp Like '" + time + "%' ORDER BY OrderID ASC";
+        ResultSet rs;
+        ArrayList<ReportSellDaliy> reportList = null;
+        rs = dbHandler.query(sql);
+        reportList = extractDaliy(rs);
+        return reportList;
+
+    }
+
+    // การหาค่าในตารางโดยหาทั้งหมด
+    public static ArrayList<ReportSellDaliy> findAllReportOfSell(DatabaseHandler dbHandler) throws SQLException {
+        String sql = "SELECT OrderID, EmployeeID, FoodName, FoodPrice, Quantity, TotalPrice, SaleTimestamp FROM SaleOrder AS s , Menu AS m WHERE s.FoodID = m.FoodID ORDER BY OrderID ASC";
+        ResultSet rs;
+        ArrayList<ReportSellDaliy> reportList = null;
+        rs = dbHandler.query(sql);
+        reportList = extractDaliy(rs);
+        return reportList;
+    }
+
+    // เป็นการนำค่าจาก database มาเก็บใว้ในรูปของ arraylist
+    private static ArrayList<ReportSellDaliy> extractDaliy(ResultSet rs) {
+        ArrayList<ReportSellDaliy> reportList = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                ReportSellDaliy report = new ReportSellDaliy();
+                try {
+                    report = new ReportSellDaliy();
+                    report.setOrderID(rs.getInt("OrderID"));
+                    report.setEmployeeID(rs.getInt("EmployeeID"));
+                    report.setFoodName(rs.getString("FoodName"));
+                    report.setFoodPrice(rs.getInt("FoodPrice"));
+                    report.setQuantity(rs.getInt("Quantity"));
+                    report.setTotalPrice(rs.getDouble("TotalPrice"));
+                    report.setSaleTimestamp(rs.getString("SaleTimestamp"));
+                } catch (SQLException ex) {
+                    Logger.getLogger(ReportTable.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                reportList.add(report);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReportTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (reportList.isEmpty()) {
+            reportList = null;
+        }
+        return reportList;
+    }
+}
